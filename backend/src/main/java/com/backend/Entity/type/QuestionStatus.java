d2aa60af18687046f9b379bf706edddf9bae94e6
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-12-03 21:37:22 +0530

    email service removed and .properties edited

--- backend/src/main/java/com/backend/Entity/type/QuestionStatus.java
+++
@@ -1,9 +0,0 @@
-package com.backend.Entity.type;
-
-public enum QuestionStatus {
-    PENDING,      // Question just submitted
-    IN_PROGRESS,  // Someone is working on answering
-    ANSWERED,     // Question has been answered
-    SPAM,         // Marked as spam
-    ARCHIVED
-}
